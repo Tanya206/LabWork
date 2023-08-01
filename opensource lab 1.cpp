6fd0466ef46807e665049b1194e52cded5e0c25f
Tanya206 <92422597+Tanya206@users.noreply.github.com>
2023-08-01 10:39:39 +0530

    Add files via upload

---
+++ opensource lab 1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+     for(int i=0; i<5; i++)
+    {
+        for(int j = 6-i; j>0; j--)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+    for(int i=0; i<6; i++)
+    {
+        for(int j = 0; j<i; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+}
